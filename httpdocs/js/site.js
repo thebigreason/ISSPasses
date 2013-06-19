b08ae2327d2a44edf3c4c55536cb4e4e892b5a77
Mark Eagleton <mark@thebigreason.com>
2013-06-18 22:28:50 -0700

    Storing night mode setting in local storage

--- httpdocs/js/site.js
+++ httpdocs/js/site.js
@@ -14,10 +14,23 @@ $(document).ready(function(){
 	}
 	*/
 	
+	// Rewrieve and apply mode from local storage
+	var mode = localStorage.getItem('mode');
+	$('body, .passes').toggleClass(mode);
+	
+	// Handle toggle mode
 	$('#night-mode').click(function(e){
 		
+		// Toggle UI
 		e.preventDefault();
 		$('body, .passes').toggleClass('night');
+		
+		// Store preference
+		if ($('body').hasClass('night')) {
+			localStorage.setItem('mode','night');
+		} else {
+			localStorage.setItem('mode','day');
+		}
 		
 	});
 
